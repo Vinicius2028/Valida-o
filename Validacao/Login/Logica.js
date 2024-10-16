edcc0a0f09eb90bf29a13ee3909215b851587837
Vinicius2028 <166714041+Vinicius2028@users.noreply.github.com>
2024-10-16 11:12:29 -0300

    Update Logica.js

--- Validacao/Login/Logica.js
+++ Validacao/Login/Logica.js
@@ -30,7 +30,7 @@ function Entrar(){
     }else if(usuario === "Jorge" && senha === "@ano221"){
         sessionStorage.setItem("Logado","Jorge")
         window.location.href = "../Home/Home.html";
-    }else if(usuario === "Julia Siviero" && senha === "@ano212"){
+    }else if(usuario === "Julia" && senha === "@ano212"){
         sessionStorage.setItem("Logado","Jorge")
         window.location.href = "../Home/Home.html";
     }else if(usuario === "Danielly" && senha === "@ano201"){
